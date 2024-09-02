233372e43ded5dd4c53f17ab123e9cfded39c918
vsctl <85422839+vsctl@users.noreply.github.com>
2024-09-02 18:40:11 -0400

    Add quotes to env.sh variables

--- env.sh
+++ env.sh
@@ -4,6 +4,6 @@ DOCKER="${DOCKER:-$(which docker)}"
 THIS_DIR="$(cd -- "$(dirname -- ${BASH_SOURCE[0]})" 2>/dev/null && pwd)"
 
 IMG_NAME="$(basename $THIS_DIR)"
-DOCKERFILE=$THIS_DIR/Dockerfile
-CONTEXT=$THIS_DIR
+DOCKERFILE="$THIS_DIR/Dockerfile"
+CONTEXT="$THIS_DIR"
 
